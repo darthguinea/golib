8a355412d526bb1c7d81a5a2937cb364932ca23b
darthguinea <darthguinea@protonmail.com>
2019-11-27 18:20:09 +1100

    Added encryption library

---
+++ enc/enc.go
@@ -0,0 +1,39 @@
+package enc
+
+func Encrypt(password *string, data []byte) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(*password)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func Decrypt(password *string, data []byte) []byte {
+	key := []byte(createHash(*password))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
+}
